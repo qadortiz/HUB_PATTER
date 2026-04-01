50a8fbfb32609e7d34ec6f23b22a14d19c1aac3d
Daniel Ortiz <dortiz@houseedge.ai>
2026-04-01 09:03:24 -0500

    Update my-organization.steps.ts

--- features/step_definitions/my-organization.steps.ts
+++ features/step_definitions/my-organization.steps.ts
@@ -4,8 +4,7 @@ import { CustomWorld } from '../../src/support/world';
 import { Logger } from '../../src/utils/Logger';
 import path from 'path';
 
-const IMAGE_PATH = 'C:\\Users\\SANIE\\Downloads\\hosfeltgallery-jean-michel-basquiat-untitled-1985 (1).jpg';
-
+const IMAGE_PATH = path.join(process.cwd(), 'Screenshot_108.png');
 // =============================================================================
 // WHEN Steps - Acciones
 // =============================================================================
